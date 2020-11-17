f08b52630aa1806d7f80b3ea9e61dfcbdd1460c6
Yazed Jamal <muyaszed@gmail.com>
2020-11-17 19:13:58 +0000

    Added test to restaurants screen

--- __tests__/config/setup.js
+++ __tests__/config/setup.js
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler/jestSetup';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
@@ -12,3 +13,4 @@ jest.mock('react-native-reanimated', () => {
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
